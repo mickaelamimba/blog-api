44c282fd91b7ba24596a41674c0ad63596bb26b2
AMIMBA Mickael <mickael.amimba@gmail.com>
2022-04-16 23:31:22 +0200

    creat post table and model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,8 +1,11 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import {BaseModel, column, hasOne,HasOne} from '@ioc:Adonis/Lucid/Orm'
+import Post from 'App/Models/Post'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
+  @hasOne(() => Post)
+  public Post: HasOne<typeof Post>
   public id: number
   @column()
   public firstName: string
