46bcb2266991785750e9b50ed451a8771df5a276
AMIMBA Mickael <mickael.amimba@gmail.com>
2022-04-17 00:28:48 +0200

    creat Category table and model

---
+++ database/migrations/1650147605985_categories.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Categories extends BaseSchema {
+  protected tableName = 'categories'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('parent_id').nullable().unsigned()
+      table.string('title',75).notNullable()
+      table.string('metaTitle',100).nullable()
+      table.string('slug',100).notNullable().unique()
+      table.text('content').nullable()
+      table.foreign('parent_id').references('categories.id')
+        .onDelete('NO ACTION').onUpdate('NO ACTION')
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
